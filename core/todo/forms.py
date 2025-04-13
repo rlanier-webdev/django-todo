b1ec799d3967a38e640e07ed9ed62f3e45c0b079
RaShunda <developer@rashundawilliams.com>
2025-04-13 13:58:42 -0500

    - moved 'is_complete' to the end of the form - add title to form widgets

--- core/todo/forms.py
+++ core/todo/forms.py
@@ -4,8 +4,9 @@ from .models import Task
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description', 'is_completed', 'priority', 'deadline']
+        fields = ['title', 'description', 'priority', 'deadline', 'is_completed']
         widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 2}),
             'priority': forms.Select(attrs={'class': 'form-control'}),  # Dropdown for priority
             'deadline': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),  # DateTime input
