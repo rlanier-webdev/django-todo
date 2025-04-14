63e1f4b32cb4e5c76e05f6da8b5efb2efd7e35b9
RaShunda <developer@rashundawilliams.com>
2025-04-13 21:01:36 -0500

    Config: Disabled timezone-aware datetime in Django
    - Set USE_TZ to False to use naive datetime objects.
    - Ensured admin and app display match server's local time.

--- core/core/settings.py
+++ core/core/settings.py
@@ -120,13 +120,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-USE_TZ = True
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
