d1276eb9f2c3b6f88adf5e0c4e796338fda6d649
RaShunda <developer@rashundawilliams.com>
2025-04-18 13:52:44 -0500

    - update session cookie age to 30mins

--- core/core/settings/base.py
+++ core/core/settings/base.py
@@ -130,6 +130,6 @@ LOGIN_REDIRECT_URL = 'app:dashboard'
 LOGOUT_REDIRECT_URL = '/'
 
 # Session expires after 30 minutes of inactivity
-SESSION_COOKIE_AGE = 10  # in seconds (30 mins)
+SESSION_COOKIE_AGE = 1800  # in seconds (30 mins)
 SESSION_SAVE_EVERY_REQUEST = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
